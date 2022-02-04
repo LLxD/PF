6e8858c729aac69aacbf8d4333f674b95c6869b4
Lucas Lima <limalucas454@gmail.com>
2022-02-04 14:50:08 +0000

    adding one compound function

--- Racket/FunctionsAndLists.rkt
+++ Racket/FunctionsAndLists.rkt
@@ -1,6 +1,12 @@
 #lang racket/base 
 (define(dobro x) (* x 2))
 (define(elevarQuadrado x) (* x x))
+(define x 3)
+(define y (+ x 1))
+ 
+(define (somaQuadrados x y)
+  (+ (elevarQuadrado x)
+     (elevarQuadrado y)))
 
 (dobro 50)
 (elevarQuadrado 100)
@@ -10,3 +16,5 @@
 (for/list ([i 5])(dobro i))
 
 (for/list ([i 5])(elevarQuadrado i))
+
+(somaQuadrados x y)
