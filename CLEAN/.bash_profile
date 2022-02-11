ae43577c358de3a00eaf792640b90f8bf087a085
Lucas Lima <limalucas454@gmail.com>
2022-02-11 13:38:04 +0000

    minor fix and adding flappy-bird file

--- CLEAN/.bash_profile
+++
@@ -1,2 +0,0 @@
-export PATH="/workspace/PF/CLEAN/bin:$PATH"
-export PATH="/workspace/PF/CLEAN/bin:$PATH"
--- CLEAN/install.sh
+++
@@ -1,4 +0,0 @@
-unzip clean.zip
-cd clean
-make
-echo "export PATH=\"$PWD/bin:\$PATH\"" >> .bash_profile
